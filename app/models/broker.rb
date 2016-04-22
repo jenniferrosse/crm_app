3993226152d63007d6997505990648dc6bf64b40
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-04-22 12:36:19 -0500

    Added search of company to brokers

--- app/models/broker.rb
+++ app/models/broker.rb
@@ -1,7 +1,7 @@
 class Broker < ActiveRecord::Base
   
   def self.search(search)
-    where("first_name ILIKE ? OR last_name ILIKE ?", "%#{search}%", "%#{search}%")
+    where("first_name ILIKE ? OR last_name ILIKE ? OR company ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
  
   end
 end
