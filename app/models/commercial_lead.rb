ce66b10537e18d4d89426b6e8749e97a09db807d
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-04-22 11:11:07 -0500

    Added ability to search based on use description for commercial leads

--- app/models/commercial_lead.rb
+++ app/models/commercial_lead.rb
@@ -1,7 +1,7 @@
 class CommercialLead < ActiveRecord::Base
 
   def self.search(search)
-    where("first_name ILIKE ? OR last_name ILIKE ? OR broker ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+    where("first_name ILIKE ? OR last_name ILIKE ? OR broker ILIKE ? OR use ILIKE ? OR description ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
  
   end
 
