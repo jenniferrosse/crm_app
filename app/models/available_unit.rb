c5d74328af5127f3b737ec341953e734c3cf6c0e
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-05-13 09:57:36 -0500

    Changed search to only prospects and edited search results page

--- app/models/available_unit.rb
+++ app/models/available_unit.rb
@@ -1,7 +1,7 @@
 class AvailableUnit < ActiveRecord::Base
 
   def self.search(search)
-    where("unit_id ILIKE ? OR market_rent ILIKE ?", "%#{search}%", "%#{search}%")
+    where("prospects ILIKE ?", "%#{search}%")
  
   end
 end
