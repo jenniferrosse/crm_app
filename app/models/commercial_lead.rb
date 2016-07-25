3e2f8a399f8c223d485221d9b5f3fd977045c9c0
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-07-25 16:01:41 -0500

    Added source search to commercial leads index page

--- app/models/commercial_lead.rb
+++ app/models/commercial_lead.rb
@@ -1,7 +1,7 @@
 class CommercialLead < ActiveRecord::Base
 
   def self.search(search)
-    where("first_name ILIKE ? OR last_name ILIKE ? OR broker ILIKE ? OR use ILIKE ? OR description ILIKE ? OR email ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+    where("first_name ILIKE ? OR last_name ILIKE ? OR broker ILIKE ? OR use ILIKE ? OR description ILIKE ? OR email ILIKE ? OR source ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
  
   end
 
