1bd2537ee69258cf58d3213ee338ef3f24e0e840
Jennifer Rosse <jenniferrosse26@gmail.com>
2018-01-04 16:44:14 -0600

    added sort to available units

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,11 @@ class PagesController < ApplicationController
   end
 
   def available_units
-    @available_units = Unit.where(available: true)
+    @available_units = Unit.where(available: true).includes(:property).order('properties.name DESC, unit_number ASC')
+    respond_to do |format|
+      format.html
+      format.csv { render text: @available_units.to_csv }
+      format.xls { send_data @available_units.to_csv(col_sep: "\t") }
+    end
   end
 end
